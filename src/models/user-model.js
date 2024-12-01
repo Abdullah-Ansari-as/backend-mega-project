fe52cf63f8468969996d9f1c28f73abc6ed7be3c
Abdullah <yousaf.8666305@gmail.com>
2024-12-02 00:44:54 +0500

    user and post model created and generate token and jwt

---
+++ src/models/user-model.js
@@ -0,0 +1,89 @@
+import mongoose, {mongo, Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+	usename: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true,
+		trim: true,
+		index: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true,
+		trim: true, 
+	},
+	fullName: {
+		type: String,
+		required: true,  
+		trim: true,
+		index: true
+	},
+	avater: {
+		type: String, // cloudinary url
+		required: true
+	},
+	coverImage: {
+		type: String, // cloudinary url 
+	},
+	watchHistory: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Video"
+		}
+	],
+	password: {
+		type: String,
+		required: [true, "password is required"]
+	},
+	refreshToken: {
+		type: String
+	}
+}, {timestamps: true});	
+
+// This code snippet is a Mongoose middleware that runs before saving a document in a MongoDB collection
+userSchema.pre("save", function (next) {
+	if(!this.isModified("password")) return next();
+
+	this.password = bcrypt.hash(this.password, 10)
+	next();
+});
+
+ 
+userSchema.methods.isPasswordCorrect = async function (password) {
+	return await bcrypt.compare(password, this.password)
+};
+
+userSchema.methods.generateAccessToken = function () {
+	return jwt.sign({
+			_id: this._id,
+			email: this.email,
+			username: this.username,
+			fullName: this.fullName 
+	    },
+		process.env.ACCESS_TOKEN_SECRET,
+		{
+			expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+		}
+	)
+};
+
+userSchema.methods.generateRefreshToken = function () {
+	return jwt.sign({
+			_id: this._id,
+	    },
+		process.env.REFRESH_TOKEN_SECRET,
+		{
+			expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+		}
+	)
+};
+
+
+
+export const User = mongoose.model("User", userSchema);
